e70ccdc56b0be25958240139f5a2cac2e4db27a4
Hazmi35 <contact@hzmi.xyz>
2023-11-17 20:23:11 +0700

    fix: lint

--- src/Structures/REST.ts
+++ src/Structures/REST.ts
@@ -3,7 +3,7 @@ import { AsyncQueue } from '@sapphire/async-queue';
 import type { RoutePlannerStatusResponse, LoadTrackResponse, LavalinkTrack, LavalinkSource } from 'lavalink-api-types';
 import { LavalinkSourceEnum, LavalinkSearchIdentifierEnum, Routes } from 'lavalink-api-types';
 import type { RequestInit } from 'undici';
-import { join } from "path";
+import { join } from 'path';
 
 export class REST {
 	public headers: { [key: string]: string } = {};
